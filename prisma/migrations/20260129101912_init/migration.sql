a4f0ba7201a0bb04f7fdabd1dad8b95c1b3b5113
diogoplates <diogo@plates-formes.ch>
2026-01-29 10:34:39 +0000

    fix app

---
+++ prisma/migrations/20260129101912_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Club" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
+);
